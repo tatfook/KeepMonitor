1b88cb984f5d665b148a95e28f055f4c19b82050
DreamAndDead <favorofife@yeah.net>
2017-08-11 15:56:39 +0800

    update

--- reverse-proxy/run.sh
+++ reverse-proxy/run.sh
@@ -11,11 +11,12 @@ if docker ps -f name=$name | grep $name; then
 fi
 
 docker run -d --name=$name -p "80:80" \
-  --link keepwork-dev-server \
-  --link keepwork-test-server \
   -v "$(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf" \
   -v "$(pwd)/nginx/keepwork.template:/etc/nginx/conf.d/keepwork.template" \
   -v "$(pwd)/nginx/html:/usr/share/nginx/html" \
+  --link keepwork-dev-server \
+  --link keepwork-test-server \
+  --link keepwork-prod-server \
   -e "DOLLAR=$$" \
   -e "DEV_ADDRESS=dev.keepwork.com" \
   -e "DEV_HOST=keepwork-dev-server" \
@@ -23,5 +24,8 @@ docker run -d --name=$name -p "80:80" \
   -e "TEST_ADDRESS=test.keepwork.com" \
   -e "TEST_HOST=keepwork-test-server" \
   -e "TEST_PORT=8099" \
+  -e "PROD_ADDRESS=keepwork.com" \
+  -e "PROD_HOST=keepwork-prod-server" \
+  -e "PROD_PORT=8088" \
   nginx \
   /bin/bash -c "envsubst </etc/nginx/conf.d/keepwork.template >/etc/nginx/conf.d/keepwork.conf && nginx -g 'daemon off;'"
