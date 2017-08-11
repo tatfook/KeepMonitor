bd1ab0f6299c06a9311522e6df27bee90b9dabfc
DreamAndDead <favorofife@yeah.net>
2017-08-11 16:04:11 +0800

    update

--- reverse-proxy/run.dev.test.sh
+++ reverse-proxy/run.dev.test.sh
@@ -12,7 +12,7 @@ fi
 
 docker run -d --name=$name -p "80:80" \
   -v "$(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf" \
-  -v "$(pwd)/nginx/keepwork.template:/etc/nginx/conf.d/keepwork.template" \
+  -v "$(pwd)/nginx/keepwork.dev.test.template:/etc/nginx/conf.d/keepwork.dev.test.template" \
   -v "$(pwd)/nginx/html:/usr/share/nginx/html" \
   --link keepwork-dev-server \
   --link keepwork-test-server \
@@ -24,8 +24,5 @@ docker run -d --name=$name -p "80:80" \
   -e "TEST_ADDRESS=test.keepwork.com" \
   -e "TEST_HOST=keepwork-test-server" \
   -e "TEST_PORT=8099" \
-  -e "PROD_ADDRESS=keepwork.com" \
-  -e "PROD_HOST=keepwork-prod-server" \
-  -e "PROD_PORT=8088" \
   nginx \
-  /bin/bash -c "envsubst </etc/nginx/conf.d/keepwork.template >/etc/nginx/conf.d/keepwork.conf && nginx -g 'daemon off;'"
+  /bin/bash -c "envsubst </etc/nginx/conf.d/keepwork.dev.test.template >/etc/nginx/conf.d/keepwork.dev.test.conf && nginx -g 'daemon off;'"
---
+++ reverse-proxy/run.prod.sh
@@ -0,0 +1,23 @@
+#! /usr/bin/env bash
+#
+# run.sh
+#
+
+name=reverse-proxy
+
+if docker ps -f name=$name | grep $name; then
+  echo "$name is running"
+  exit 0
+fi
+
+docker run -d --name=$name -p "80:80" \
+  -v "$(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf" \
+  -v "$(pwd)/nginx/keepwork.prod.template:/etc/nginx/conf.d/keepwork.prod.template" \
+  -v "$(pwd)/nginx/html:/usr/share/nginx/html" \
+  --link keepwork-prod-server \
+  -e "DOLLAR=$$" \
+  -e "PROD_ADDRESS=keepwork.com" \
+  -e "PROD_HOST=keepwork-prod-server" \
+  -e "PROD_PORT=8088" \
+  nginx \
+  /bin/bash -c "envsubst </etc/nginx/conf.d/keepwork.prod.template >/etc/nginx/conf.d/keepwork.prod.conf && nginx -g 'daemon off;'"
