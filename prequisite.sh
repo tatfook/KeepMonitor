9a02c17e10e5d977cb157dbb2f471266d7cf3f57
DreamAndDead <favorofife@yeah.net>
2017-08-15 10:30:02 +0800

    update script

---
+++ prequisite.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+#
+#
+# need to be root
+
+
+# install docker
+if ! command docker &>/dev/null; then
+  apt-get remove docker docker-engine docker.io
+
+  apt-get update
+  sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  apt-key fingerprint 0EBFCD88
+
+  add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+  apt-get update
+  apt-get install docker-ce
+fi
+
+# install docker-compose
+DOCKER_COMPOSE_PATH=/usr/local/bin/docker-compose
+if [[ ! -f $DOCKER_COMPOSE_PATH ]]; then
+  curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > $DOCKER_COMPOSE_PATH
+  chmod +x $DOCKER_COMPOSE_PATH
+fi
+
+
+
+
+
+
